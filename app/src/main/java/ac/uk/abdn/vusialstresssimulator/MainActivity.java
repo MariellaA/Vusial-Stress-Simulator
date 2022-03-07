38e74e67e5906a34818651b02e9dc7c0b41fdb87
Mariela <u12ma18@abdn.ac.uk>
2022-03-07 01:37:01 +0000

    Add Welcoming Message on Startup, Fix Layouts' Widget margins

--- app/src/main/java/ac/uk/abdn/vusialstresssimulator/MainActivity.java
+++ app/src/main/java/ac/uk/abdn/vusialstresssimulator/MainActivity.java
@@ -2,13 +2,25 @@ package ac.uk.abdn.vusialstresssimulator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static int SPLASH_TIME_OUT = 3500;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.dummy_text_sim);
+        setContentView(R.layout.activity_main);
+        new Handler().postDelayed(new Runnable() {     // Displays an image for 4 sec before loading the Main Menu
+            @Override
+            public void run() {
+                Intent greetIntent = new Intent(MainActivity.this, MainMenuActivity.class);
+                startActivity(greetIntent);
+                // finish;
+            }
+        }, SPLASH_TIME_OUT);
     }
 }
